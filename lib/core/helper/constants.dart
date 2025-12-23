270d007c589130aee3ee165ba0204c4c9d7e7966
Ahmed Adel <ahmedadelelmelegy2@gmail.com>
2025-12-23 10:01:14 +0200

    improve image convert image to webp

--- lib/core/helper/constants.dart
+++ lib/core/helper/constants.dart
@@ -1,115 +1,115 @@
 class AppImages {
-  static const String project1 = 'assets/images/project1.png';
-  static const String project2 = 'assets/images/project3.png';
-  static const String project3 = 'assets/images/project2.png';
-  static const String project4 = 'assets/images/project4.png';
+  static const String project1 = 'assets/images/project1.webp';
+  static const String project2 = 'assets/images/project3.webp';
+  static const String project3 = 'assets/images/project2.webp';
+  static const String project4 = 'assets/images/project4.webp';
   // for small image
   // project1Sm
 
   static const String smImageProject11 =
-      'assets/images/sm_image/project1/project11.png';
+      'assets/images/sm_image/project1/project11.webp';
   static const String smImageProject12 =
-      'assets/images/sm_image/project1/project12.png';
+      'assets/images/sm_image/project1/project12.webp';
   static const String smImageProject13 =
-      'assets/images/sm_image/project1/project13.png';
+      'assets/images/sm_image/project1/project13.webp';
   static const String smImageProject14 =
-      'assets/images/sm_image/project1/project14.png';
+      'assets/images/sm_image/project1/project14.webp';
   static const String smImageProject15 =
-      'assets/images/sm_image/project1/project15.png';
+      'assets/images/sm_image/project1/project15.webp';
   static const String smImageProject16 =
-      'assets/images/sm_image/project1/project16.png';
+      'assets/images/sm_image/project1/project16.webp';
   static const String smImageProject17 =
-      'assets/images/sm_image/project1/project17.png';
+      'assets/images/sm_image/project1/project17.webp';
   static const String smImageProject18 =
-      'assets/images/sm_image/project1/project18.png';
+      'assets/images/sm_image/project1/project18.webp';
   static const String smImageProject19 =
-      'assets/images/sm_image/project1/project19.png';
+      'assets/images/sm_image/project1/project19.webp';
   static const String smImageProject20 =
-      'assets/images/sm_image/project1/project20.png';
+      'assets/images/sm_image/project1/project20.webp';
   static const String smImageProject21 =
-      'assets/images/sm_image/project1/project21.png';
+      'assets/images/sm_image/project1/project21.webp';
   static const String smImageProject22 =
-      'assets/images/sm_image/project1/project22.png';
+      'assets/images/sm_image/project1/project22.webp';
   static const String smImageProject23 =
-      'assets/images/sm_image/project1/project23.png';
+      'assets/images/sm_image/project1/project23.webp';
   static const String smImageProject24 =
-      'assets/images/sm_image/project1/project24.png';
+      'assets/images/sm_image/project1/project24.webp';
   static const String smImageProject25 =
-      'assets/images/sm_image/project1/project25.png';
+      'assets/images/sm_image/project1/project25.webp';
   static const String smImageProject26 =
-      'assets/images/sm_image/project1/project26.png';
+      'assets/images/sm_image/project1/project26.webp';
   static const String smImageProject27 =
-      'assets/images/sm_image/project1/project27.png';
+      'assets/images/sm_image/project1/project27.webp';
   static const String smImageProject28 =
-      'assets/images/sm_image/project28/project28.png';
+      'assets/images/sm_image/project28/project28.webp';
 
   //  sm for 3   //
   // 31 to 36
 
   static const String smImageProject31 =
-      'assets/images/sm_image/project3/project31.png';
+      'assets/images/sm_image/project3/project31.webp';
   static const String smImageProject32 =
-      'assets/images/sm_image/project3/project32.png';
+      'assets/images/sm_image/project3/project32.webp';
   static const String smImageProject33 =
-      'assets/images/sm_image/project3/project33.png';
+      'assets/images/sm_image/project3/project33.webp';
   static const String smImageProject34 =
-      'assets/images/sm_image/project3/project34.png';
+      'assets/images/sm_image/project3/project34.webp';
   static const String smImageProject35 =
-      'assets/images/sm_image/project3/project35.png';
+      'assets/images/sm_image/project3/project35.webp';
   static const String smImageProject36 =
-      'assets/images/sm_image/project3/project36.png';
+      'assets/images/sm_image/project3/project36.webp';
 
   // 41 to 42
   static const String smImageProject41 =
-      'assets/images/sm_image/project4/project41.png';
+      'assets/images/sm_image/project4/project41.webp';
   static const String smImageProject42 =
-      'assets/images/sm_image/project4/project42.png';
+      'assets/images/sm_image/project4/project42.webp';
 
   // 21 to43
   static const String smImageProjectQ21 =
-      'assets/images/sm_image/project2/21.png';
+      'assets/images/sm_image/project2/21.webp';
   static const String smImageProjectQ22 =
-      'assets/images/sm_image/project2/22.png';
+      'assets/images/sm_image/project2/22.webp';
   static const String smImageProjectQ23 =
-      'assets/images/sm_image/project2/23.png';
+      'assets/images/sm_image/project2/23.webp';
   static const String smImageProjectQ24 =
-      'assets/images/sm_image/project2/24.png';
+      'assets/images/sm_image/project2/24.webp';
   static const String smImageProjectQ25 =
-      'assets/images/sm_image/project2/25.png';
+      'assets/images/sm_image/project2/25.webp';
   static const String smImageProjectQ26 =
-      'assets/images/sm_image/project2/26.png';
+      'assets/images/sm_image/project2/26.webp';
   static const String smImageProjectQ27 =
-      'assets/images/sm_image/project2/27.png';
+      'assets/images/sm_image/project2/27.webp';
   static const String smImageProjectQ28 =
-      'assets/images/sm_image/project2/28.png';
+      'assets/images/sm_image/project2/28.webp';
   static const String smImageProjectQ29 =
-      'assets/images/sm_image/project2/29.png';
+      'assets/images/sm_image/project2/29.webp';
   static const String smImageProjectQ30 =
-      'assets/images/sm_image/project2/30.png';
+      'assets/images/sm_image/project2/30.webp';
   static const String smImageProjectQ31 =
-      'assets/images/sm_image/project2/31.png';
+      'assets/images/sm_image/project2/31.webp';
   static const String smImageProjectQ32 =
-      'assets/images/sm_image/project2/32.png';
+      'assets/images/sm_image/project2/32.webp';
   static const String smImageProjectQ33 =
-      'assets/images/sm_image/project2/33.png';
+      'assets/images/sm_image/project2/33.webp';
   static const String smImageProjectQ34 =
-      'assets/images/sm_image/project2/34.png';
+      'assets/images/sm_image/project2/34.webp';
   static const String smImageProjectQ35 =
-      'assets/images/sm_image/project`2/35.png';
+      'assets/images/sm_image/project`2/35.webp';
   static const String smImageProjectQ36 =
-      'assets/images/sm_image/project2/36.png';
+      'assets/images/sm_image/project2/36.webp';
   static const String smImageProjectQ37 =
-      'assets/images/sm_image/project2/37.png';
+      'assets/images/sm_image/project2/37.webp';
   static const String smImageProjectQ38 =
-      'assets/images/sm_image/project2/38.png';
+      'assets/images/sm_image/project2/38.webp';
   static const String smImageProjectQ39 =
-      'assets/images/sm_image/project2/39.png';
+      'assets/images/sm_image/project2/39.webp';
   static const String smImageProjectQ40 =
-      'assets/images/sm_image/project2/40.png';
+      'assets/images/sm_image/project2/40.webp';
   static const String smImageProjectQ41 =
-      'assets/images/sm_image/project2/41.png';
+      'assets/images/sm_image/project2/41.webp';
   static const String smImageProjectQ42 =
-      'assets/images/sm_image/project2/42.png';
+      'assets/images/sm_image/project2/42.webp';
   static const String smImageProjectQ43 =
-      'assets/images/sm_image/project2/43.png';
+      'assets/images/sm_image/project2/43.webp';
 }
